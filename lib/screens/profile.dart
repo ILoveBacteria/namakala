052434fecf6d87470c7a7a86a20ab3102e39cd9b
Moein Arabi <moein.mo81@gmail.com>
2022-05-22 09:49:05 +0430

    Improved design profile screen

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -7,86 +7,73 @@ class Profile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.only(
-            top: 50.0,
-            bottom: 10.0
+    return ScreenSetting.initScreen(
+      context: context,
+      child: Column(
+        children: [
+          ScreenSetting.buildTitleText('Profile'),
+          const SizedBox(height: 30.0),
+          Container(
+            width: 100.0,
+            height: 100.0,
+            decoration: BoxDecoration(
+              border: Border.all(),
+              shape: BoxShape.circle,
+            ),
+            child: const Icon(
+              Icons.person,
+              size: 50.0,
+            ),
           ),
-          child: Column(
-            children: [
-              ScreenSetting.buildTitleText('Profile'),
-              const SizedBox(height: 30.0),
-              Container(
-                width: 100.0,
-                height: 100.0,
-                decoration: BoxDecoration(
-                  border: Border.all(),
-                  shape: BoxShape.circle,
-                ),
-                child: const Icon(
-                  Icons.person,
-                  size: 50.0,
-                ),
-              ),
-              const SizedBox(height: 10.0),
-              Text(
-                'Moein Arabi',
-                style: GoogleFonts.nunito(
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.w700,
-                ),
-              ),
-              const SizedBox(height: 5.0),
-              Text(
-                '+989120284896',
-                style: GoogleFonts.nunito(
-                  fontSize: 15.0,
-                  color: Colors.grey,
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
-              const SizedBox(height: 20.0,),
-              Container(
-                padding: const EdgeInsets.all(20.0),
-                width: double.infinity,
-                // height: 300.0,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(30.0),
-                  color: Colors.black26,
-                ),
-                child: Column(
-                  children: [
-                    _button(
-                        text: 'Account Information',
-                        icon: Icons.account_circle_outlined,
-                        onPressed: () {}
-                    ),
-                    const SizedBox(height: 5.0),
-                    _button(
-                        text: 'Purchase History',
-                        icon: Icons.history,
-                        onPressed: () {}
-                    ),
-                    const SizedBox(height: 5.0),
-                    _button(
-                        text: 'My Market',
-                        icon: Icons.store_outlined,
-                        onPressed: () {}
-                    ),
-                    const SizedBox(height: 5.0),
-                    _button(
-                        text: 'Favorites',
-                        icon: Icons.favorite_outline,
-                        onPressed: () {}
-                    ),
-                  ],
-                ),
-              ),
-            ],
+          const SizedBox(height: 10.0),
+          Text(
+            'Moein Arabi',
+            style: GoogleFonts.nunito(
+              fontSize: 20.0,
+              fontWeight: FontWeight.w700,
+            ),
           ),
-        ),
+          const SizedBox(height: 5.0),
+          Text(
+            '+989120284896',
+            style: GoogleFonts.nunito(
+              fontSize: 15.0,
+              color: Colors.grey,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          const SizedBox(height: 20.0,),
+          Container(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              children: [
+                _button(
+                    text: 'Account Information',
+                    icon: Icons.account_circle_outlined,
+                    onPressed: () {}
+                ),
+                const SizedBox(height: 5.0),
+                _button(
+                    text: 'Purchase History',
+                    icon: Icons.history,
+                    onPressed: () {}
+                ),
+                const SizedBox(height: 5.0),
+                _button(
+                    text: 'My Market',
+                    icon: Icons.store_outlined,
+                    onPressed: () {}
+                ),
+                const SizedBox(height: 5.0),
+                _button(
+                    text: 'Favorites',
+                    icon: Icons.favorite_outline,
+                    onPressed: () {}
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
@@ -101,15 +88,18 @@ class Profile extends StatelessWidget {
       child: Container(
         padding: const EdgeInsets.only(left: 15.0, right: 15.0),
         width: double.infinity,
-        height: 50.0,
+        height: 55.0,
         decoration: BoxDecoration(
-          color: Colors.white,
+          border: Border.all(
+            color: Colors.black12,
+          ),
           borderRadius: BorderRadius.circular(30.0),
         ),
         child: Row(
           children: [
             Icon(
               icon,
+              color: Colors.black54,
               size: 30.0,
             ),
             const SizedBox(width: 15.0,),
@@ -117,12 +107,14 @@ class Profile extends StatelessWidget {
               text,
               style: GoogleFonts.montserrat(
                 fontSize: 16.0,
+                color: Colors.black54,
                 fontWeight: FontWeight.w500,
               ),
             ),
             const Spacer(),
             const Icon(
               Icons.navigate_next,
+              color: Colors.grey,
               size: 30.0,
             )
           ],
