d2638388f6a8ddeb81d1b0919d03791db52081bd
Moein Arabi <moein.mo81@gmail.com>
2022-05-25 17:45:25 +0430

    Added label to fields and changed hint texts

--- lib/widgets/field.dart
+++ lib/widgets/field.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import '../utilities/decoration.dart';
@@ -5,7 +6,7 @@ import '../utilities/decoration.dart';
 class Field {
   static hintStyle() {
     return GoogleFonts.nunito(
-      color: Colors.black38,
+      color: Colors.black26,
     );
   }
 
@@ -19,7 +20,7 @@ class Field {
 
   static container(Widget child) {
     return Container(
-        height: 60.0,
+        height: 55.0,
         alignment: Alignment.centerLeft,
         padding: const EdgeInsets.only(left: 3.0),
         decoration: fieldDecoration(),
@@ -28,86 +29,122 @@ class Field {
   }
 
   static email() {
-    return Field.container(
-      TextField(
-        keyboardType: TextInputType.emailAddress,
-        decoration: InputDecoration(
-          hintText: 'Email',
-          hintStyle: Field.hintStyle(),
-          border: Field.border(),
-          prefixIcon: const Icon(Icons.mail_outline),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        label(label: 'Email'),
+        Field.container(
+          TextField(
+            keyboardType: TextInputType.emailAddress,
+            decoration: InputDecoration(
+              hintText: 'example@mail.com',
+              hintStyle: Field.hintStyle(),
+              border: Field.border(),
+              prefixIcon: const Icon(Icons.mail_outline),
+            ),
+          )
         ),
-      )
+      ],
     );
   }
 
   static password() {
-    return Field.container(
-      TextField(
-        obscureText: true,
-        decoration: InputDecoration(
-          hintText: 'Password',
-          hintStyle: Field.hintStyle(),
-          border: Field.border(),
-          prefixIcon: const Icon(Icons.password_outlined),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        label(label: 'Password'),
+        Field.container(
+          TextField(
+            obscureText: true,
+            decoration: InputDecoration(
+              hintText: 'Contains A-z and 0-9',
+              hintStyle: Field.hintStyle(),
+              border: Field.border(),
+              prefixIcon: const Icon(Icons.password_outlined),
+            ),
+          )
         ),
-      )
+      ],
     );
   }
 
   static passwordConfirm() {
-    return Field.container(
-      TextField(
-        obscureText: true,
-        decoration: InputDecoration(
-          hintText: 'Confirm Password',
-          hintStyle: Field.hintStyle(),
-          border: Field.border(),
-          prefixIcon: const Icon(Icons.password_outlined),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        label(label: 'Confirm Password'),
+        Field.container(
+          TextField(
+            obscureText: true,
+            decoration: InputDecoration(
+              hintText: 'Contains A-z and 0-9',
+              hintStyle: Field.hintStyle(),
+              border: Field.border(),
+              prefixIcon: const Icon(Icons.password_outlined),
+            ),
+          )
         ),
-      )
+      ],
     );
   }
 
   static firstName() {
-    return Field.container(
-      TextField(
-        keyboardType: TextInputType.name,
-        decoration: InputDecoration(
-          hintText: 'First Name',
-          hintStyle: Field.hintStyle(),
-          border: Field.border(),
-          prefixIcon: const Icon(Icons.badge_outlined),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        label(label: 'First Name'),
+        Field.container(
+          TextField(
+            keyboardType: TextInputType.name,
+            decoration: InputDecoration(
+              hintText: 'Lionel',
+              hintStyle: Field.hintStyle(),
+              border: Field.border(),
+              prefixIcon: const Icon(Icons.badge_outlined),
+            ),
+          )
         ),
-      )
+      ],
     );
   }
 
   static lastName() {
-    return Field.container(
-      TextField(
-        keyboardType: TextInputType.name,
-        decoration: InputDecoration(
-          hintText: 'Last Name',
-          hintStyle: Field.hintStyle(),
-          border: Field.border(),
-          prefixIcon: const Icon(Icons.badge_outlined),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        label(label: 'Last Name'),
+        Field.container(
+          TextField(
+            keyboardType: TextInputType.name,
+            decoration: InputDecoration(
+              hintText: 'Messi',
+              hintStyle: Field.hintStyle(),
+              border: Field.border(),
+              prefixIcon: const Icon(Icons.badge_outlined),
+            ),
+          )
         ),
-      )
+      ],
     );
   }
 
   static phone() {
-    return Field.container(
-      TextField(
-        keyboardType: TextInputType.phone,
-        decoration: InputDecoration(
-          hintText: 'Phone',
-          hintStyle: Field.hintStyle(),
-          border: Field.border(),
-          prefixIcon: const Icon(Icons.call_outlined),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        label(label: 'Phone'),
+        Field.container(
+          TextField(
+            keyboardType: TextInputType.phone,
+            decoration: InputDecoration(
+              hintText: '+989120284896',
+              hintStyle: Field.hintStyle(),
+              border: Field.border(),
+              prefixIcon: const Icon(Icons.call_outlined),
+            ),
+          )
         ),
-      )
+      ],
     );
   }
 
@@ -117,6 +154,17 @@ class Field {
       padding: const EdgeInsets.all(20.0),
       decoration: containerDecoration(),
       child: child
+    );
+  }
+
+  static Text label({required String label}) {
+    return Text(
+      label,
+      style: GoogleFonts.nunito(
+        color: Colors.grey,
+        fontSize: 17.0,
+        fontWeight: FontWeight.w700
+      ),
     );
   }
 }
