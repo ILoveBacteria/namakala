3e111716ee77fbe7381e3e2ff9b1d91f33fefc39
Moein Arabi <moein.mo81@gmail.com>
2022-05-25 17:44:51 +0430

    Added onPress function to edit button

--- lib/screens/account_information.dart
+++ lib/screens/account_information.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:namakala/widgets/button.dart';
+import 'package:namakala/widgets/field.dart';
 import 'package:namakala/widgets/screen_setting.dart';
 
 class Account extends StatefulWidget {
@@ -11,47 +12,26 @@ class Account extends StatefulWidget {
 }
 
 class _AccountState extends State<Account> {
+  bool _editScreen = false;
+
   @override
   Widget build(BuildContext context) {
     return ScreenSetting.initScreenWithSliverBar(
-      appBar: ScreenSetting.sliverAppBar(),
+      appBar: ScreenSetting.sliverAppBar(context: context),
       context: context,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          _userInformation(label: 'Name', text: 'Moein Arabi'),
-          const Divider(color: Colors.grey),
-          _userInformation(label: 'Phone', text: '+989120284896'),
-          const Divider(color: Colors.grey),
-          _userInformation(label: 'Email', text: 'moein.mo81@gmail.com'),
-          const Divider(color: Colors.grey),
-          _userInformation(label: 'Password', text: 'wsds#vd45'),
-          Button.separate(),
-          Center(
-            child: Button.elevatedIcon(
-              icon: Icons.edit,
-              label: 'EDIT',
-              color: Colors.blueGrey,
-              onPress: () {}
-            ),
-          )
-        ],
-      ),
+      child: _mainStateWidget(),
     );
+  }
+
+  Widget _mainStateWidget() {
+    return _editScreen ? _userEditInformationWidgets() : _userInformationWidgets();
   }
 
   Widget _userInformation({required String label, required String text}) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text(
-          label,
-          style: GoogleFonts.nunito(
-              color: Colors.grey,
-              fontSize: 17.0,
-              fontWeight: FontWeight.w700
-          ),
-        ),
+        Field.label(label: label),
         const SizedBox(height: 5.0),
         Text(
           text,
@@ -63,5 +43,35 @@ class _AccountState extends State<Account> {
         ),
       ],
     );
+  }
+
+  Widget _userInformationWidgets() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        _userInformation(label: 'Name', text: 'Moein Arabi'),
+        const Divider(color: Colors.grey),
+        _userInformation(label: 'Phone', text: '+989120284896'),
+        const Divider(color: Colors.grey),
+        _userInformation(label: 'Email', text: 'moein.mo81@gmail.com'),
+        const Divider(color: Colors.grey),
+        _userInformation(label: 'Password', text: 'wsds#vd45'),
+        Button.separate(),
+        Center(
+          child: Button.elevatedIcon(
+            icon: Icons.edit,
+            label: 'EDIT',
+            color: Colors.blueGrey,
+            onPress: () => setState(() {
+              _editScreen = !_editScreen;
+            }),
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget _userEditInformationWidgets() {
+    return Container();
   }
 }
