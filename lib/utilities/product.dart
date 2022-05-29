5a683b1009272be20d8018a8e8442f853fb1dd73
Moein Arabi <moein.mo81@gmail.com>
2022-05-29 19:15:19 +0430

    Changed private fields to public

--- lib/utilities/product.dart
+++ lib/utilities/product.dart
@@ -1,25 +1,7 @@
 class Product {
-  String _name;
-  String _image;
-  double _price;
+  String name;
+  String image;
+  int price;
 
-  Product(this._name, this._image, this._price);
-
-  double get price => _price;
-
-  String get image => _image;
-
-  String get name => _name;
-
-  set price(double value) {
-    _price = value;
-  }
-
-  set image(String value) {
-    _image = value;
-  }
-
-  set name(String value) {
-    _name = value;
-  }
+  Product(this.name, this.image, this.price);
 }
