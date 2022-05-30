8959483dd959a77ca86642e03e2023a717b8a03e
Moein Arabi <moein.mo81@gmail.com>
2022-05-30 12:10:24 +0430

    Added category field

--- lib/utilities/product.dart
+++ lib/utilities/product.dart
@@ -2,6 +2,15 @@ class Product {
   String name;
   String image;
   int price;
+  String category;
 
-  Product(this.name, this.image, this.price);
+  static final List<Product> mobileProducts = <Product>[
+    Product('iPhone 13 pro max - 512Gb', 'assets/images/iphone.png', 999, 'Mobiles'),
+    Product('iPhone 13 - 256Gb', 'assets/images/iphone13.png', 899, 'Mobiles'),
+    Product('iPhone 13 pro max - 512Gb', 'assets/images/iphone.png', 999, 'Mobiles'),
+    Product('iPhone 13 - 256Gb', 'assets/images/iphone13.png', 899, 'Mobiles'),
+    Product('iPhone 13 pro max - 512Gb', 'assets/images/iphone.png', 999, 'Mobiles'),
+  ];
+
+  Product(this.name, this.image, this.price, this.category);
 }
