9d2fd2b7a8d474934a5f4e9e0cf926a3c7a12d6c
Moein Arabi <moein.mo81@gmail.com>
2022-05-30 15:29:31 +0430

    Added equal and hashcode methods

--- lib/utilities/product.dart
+++ lib/utilities/product.dart
@@ -1,8 +1,12 @@
+import 'package:namakala/utilities/market.dart';
+
 class Product {
   String name;
   String image;
   int price;
   String category;
+  Market? market;
+  int? count;
 
   static final List<Product> mobileProducts = <Product>[
     Product('iPhone 13 pro max - 512Gb', 'assets/images/iphone.png', 999, 'Mobiles'),
@@ -20,4 +24,23 @@ class Product {
   ];
 
   Product(this.name, this.image, this.price, this.category);
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is Product &&
+          runtimeType == other.runtimeType &&
+          name == other.name &&
+          image == other.image &&
+          price == other.price &&
+          category == other.category &&
+          market == other.market;
+
+  @override
+  int get hashCode =>
+      name.hashCode ^
+      image.hashCode ^
+      price.hashCode ^
+      category.hashCode ^
+      market.hashCode;
 }
