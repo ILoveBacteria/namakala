8709b3b26181e6d33c847ef456e1d2343b1279da
Moein Arabi <moein.mo81@gmail.com>
2022-05-31 22:42:45 +0430

    Added size and color field

--- lib/utilities/product.dart
+++ lib/utilities/product.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:namakala/utilities/market.dart';
 
 class Product {
@@ -10,23 +11,25 @@ class Product {
   int? count;
   double score = 0;
   int _countVoters = 0;
+  Color color;
+  String? size;
 
   static final List<Product> mobileProducts = <Product>[
-    Product('iPhone 13 pro max - 512Gb', 'assets/images/iphone.png', 999, 'Mobiles', 'This product is in Test!'),
-    Product('iPhone 13 - 256Gb', 'assets/images/iphone13.png', 899, 'Mobiles', 'This product is in Test!'),
-    Product('iPhone 13 pro max - 512Gb', 'assets/images/iphone.png', 999, 'Mobiles', 'This product is in Test!'),
-    Product('iPhone 13 - 256Gb', 'assets/images/iphone13.png', 899, 'Mobiles', 'This product is in Test!'),
-    Product('iPhone 13 pro max - 512Gb', 'assets/images/iphone.png', 999, 'Mobiles', 'This product is in Test!'),
+    Product('iPhone 13 pro max - 512Gb', 'assets/images/iphone.png', 999, 'Mobiles', 'This product is in Test!', Colors.blue),
+    Product('iPhone 13 - 256Gb', 'assets/images/iphone13.png', 899, 'Mobiles', 'This product is in Test!', Colors.blue),
+    Product('iPhone 13 pro max - 512Gb', 'assets/images/iphone.png', 999, 'Mobiles', 'This product is in Test!', Colors.blue),
+    Product('iPhone 13 - 256Gb', 'assets/images/iphone13.png', 899, 'Mobiles', 'This product is in Test!', Colors.blue),
+    Product('iPhone 13 pro max - 512Gb', 'assets/images/iphone.png', 999, 'Mobiles', 'This product is in Test!', Colors.blue),
   ];
 
   static final List<Product> laptopProducts = <Product>[
-    Product('Macbook Air', 'assets/images/macbook.png', 999, 'Laptops', 'This product is in Test!'),
-    Product('MacBook Pro M1', 'assets/images/macbook.png', 1199, 'Laptops', 'This product is in Test!'),
-    Product('Macbook Air', 'assets/images/macbook.png', 999, 'Laptops', 'This product is in Test!'),
-    Product('MacBook Pro M1', 'assets/images/macbook.png', 1199, 'Laptops', 'This product is in Test!'),
+    Product('Macbook Air', 'assets/images/macbook.png', 999, 'Laptops', 'This product is in Test!', Colors.blue),
+    Product('MacBook Pro M1', 'assets/images/macbook.png', 1199, 'Laptops', 'This product is in Test!', Colors.blue),
+    Product('Macbook Air', 'assets/images/macbook.png', 999, 'Laptops', 'This product is in Test!', Colors.blue),
+    Product('MacBook Pro M1', 'assets/images/macbook.png', 1199, 'Laptops', 'This product is in Test!', Colors.blue),
   ];
 
-  Product(this.name, this.image, this.price, this.category, this.detail);
+  Product(this.name, this.image, this.price, this.category, this.detail, this.color);
 
   @override
   bool operator ==(Object other) =>
