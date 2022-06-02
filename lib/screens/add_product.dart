d74184b9bf30de4d1bfa4bc2f5873bc55c165866
Moein Arabi <moein.mo81@gmail.com>
2022-06-02 12:37:28 +0430

    Added focus node to fields

--- lib/screens/add_product.dart
+++ lib/screens/add_product.dart
@@ -13,6 +13,21 @@ class AddProduct extends StatefulWidget {
 class _AddProductState extends State<AddProduct> {
   List<Widget> _sizeChips = <Widget>[];
   List _size = [];
+  final FocusNode _titleFocus = FocusNode();
+  final FocusNode _priceFocus = FocusNode();
+  final FocusNode _marketFocus = FocusNode();
+  final FocusNode _sizeFocus = FocusNode();
+  final FocusNode _detailFocus = FocusNode();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _titleFocus.dispose();
+    _priceFocus.dispose();
+    _marketFocus.dispose();
+    _sizeFocus.dispose();
+    _detailFocus.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,15 +38,49 @@ class _AddProductState extends State<AddProduct> {
         appBar: ScreenSetting.appBar(title: 'Add A New Product', context: context),
         child: Column(
           children: [
-            Field.field(label: 'Product title', hintText: 'iPhone 13', prefixIcon: Icons.title_outlined),
+            Field.field(
+              label: 'Product title',
+              hintText: 'iPhone 13',
+              prefixIcon: Icons.title_outlined,
+              focusNode: _titleFocus,
+              onTap: () => setState(() {}),
+            ),
             Field.separate(),
-            Field.field(label: 'Price', hintText: '599' ,keyboardType: TextInputType.number, prefixIcon: Icons.attach_money),
+            Field.field(
+              label: 'Price',
+              hintText: '599',
+              keyboardType: TextInputType.number,
+              prefixIcon: Icons.attach_money,
+              focusNode: _priceFocus,
+              onTap: () => setState(() {}),
+            ),
             Field.separate(),
-            Field.field(label: 'Market', initialValue: 'person.market.name', hintText: 'Digikala', prefixIcon: Icons.store_outlined,),
+            Field.field(
+              label: 'Market',
+              initialValue: 'person.market.name',
+              hintText: 'Digikala',
+              prefixIcon: Icons.store_outlined,
+              focusNode: _marketFocus,
+              onTap: () => setState(() {}),
+            ),
             Field.separate(),
-            Field.field(label: 'Size', hintText: 'Write your size then press done button', prefixIcon: Icons.straighten_outlined),
+            Field.field(
+              label: 'Size',
+              hintText: 'Write your size then press done button',
+              prefixIcon: Icons.straighten_outlined,
+              focusNode: _sizeFocus,
+              onTap: () => setState(() {}),
+            ),
             Field.separate(),
-            Field.field(label: 'More Detail', hintText: 'Write more information about your product...', keyboardType: TextInputType.multiline, maxLines: 10, height: 200.0),
+            Field.field(
+              label: 'More Detail',
+              hintText: 'Write more information about your product...',
+              keyboardType: TextInputType.multiline,
+              maxLines: 10,
+              height: 200.0,
+              focusNode: _detailFocus,
+              onTap: () => setState(() {}),
+            ),
           ],
         ),
       ),
