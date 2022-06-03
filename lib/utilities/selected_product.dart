262b330f5d9f33b015ce7c96ed9c08598e8662e9
Moein Arabi <moein.mo81@gmail.com>
2022-06-03 15:23:36 +0430

    Added SelectedProduct class

---
+++ lib/utilities/selected_product.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:namakala/utilities/product.dart';
+
+class SelectedProduct {
+  Product product;
+  Color color;
+  String? size;
+
+  SelectedProduct(this.product, this.color, this.size);
+}
