7bbf10bffc9432c8b664736bf1b54a44d4c4ebe1
Moein Arabi <moein.mo81@gmail.com>
2022-06-03 15:24:32 +0430

    Removed sample products and changed fields

--- lib/utilities/product.dart
+++ lib/utilities/product.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:namakala/data/sample_data.dart';
 import 'package:namakala/utilities/market.dart';
+import 'package:namakala/utilities/person.dart';
 
 class Product {
   String name;
@@ -7,29 +9,15 @@ class Product {
   String detail;
   int price;
   String category;
-  Market? market;
+  Market market;
   int? count;
   double score = 0;
   int _countVoters = 0;
-  Color color;
-  String? size;
+  List<Color> color = [];
+  List<String> size = [];
 
-  static final List<Product> mobileProducts = <Product>[
-    Product('iPhone 13 pro max - 512Gb', 'assets/images/iphone.png', 999, 'Mobiles', 'This product is in Test!', Colors.blue),
-    Product('iPhone 13 - 256Gb', 'assets/images/iphone13.png', 899, 'Mobiles', 'This product is in Test!', Colors.blue),
-    Product('iPhone 13 pro max - 512Gb', 'assets/images/iphone.png', 999, 'Mobiles', 'This product is in Test!', Colors.blue),
-    Product('iPhone 13 - 256Gb', 'assets/images/iphone13.png', 899, 'Mobiles', 'This product is in Test!', Colors.blue),
-    Product('iPhone 13 pro max - 512Gb', 'assets/images/iphone.png', 999, 'Mobiles', 'This product is in Test!', Colors.blue),
-  ];
-
-  static final List<Product> laptopProducts = <Product>[
-    Product('Macbook Air', 'assets/images/macbook.png', 999, 'Laptops', 'This product is in Test!', Colors.blue),
-    Product('MacBook Pro M1', 'assets/images/macbook.png', 1199, 'Laptops', 'This product is in Test!', Colors.blue),
-    Product('Macbook Air', 'assets/images/macbook.png', 999, 'Laptops', 'This product is in Test!', Colors.blue),
-    Product('MacBook Pro M1', 'assets/images/macbook.png', 1199, 'Laptops', 'This product is in Test!', Colors.blue),
-  ];
-
-  Product(this.name, this.image, this.price, this.category, this.detail, this.color);
+  Product(this.name, this.image, this.price, this.category, this.detail,
+      this.color, this.market);
 
   @override
   bool operator ==(Object other) =>
