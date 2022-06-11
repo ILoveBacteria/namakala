b7a2a2d102a5790710cf10212c19434a840e8b78
Moein Arabi <moein.mo81@gmail.com>
2022-06-11 17:16:17 +0430

    Getting and sending data

--- src/server/AcceptClient.java
+++ src/server/AcceptClient.java
@@ -1,6 +1,9 @@
 package server;
 
+import java.io.IOException;
 import java.net.Socket;
+import java.util.Formatter;
+import java.util.Scanner;
 
 public class AcceptClient implements Runnable {
     Socket socket;
@@ -11,6 +14,16 @@ public class AcceptClient implements Runnable {
     
     @Override
     public void run() {
-    
+        try (Scanner in = new Scanner(socket.getInputStream(), "UTF-8");
+             Formatter out = new Formatter(socket.getOutputStream(), "UTF-8")) {
+            
+            String input = in.nextLine();
+            Command inputCommand = Command.parseCommand(input);
+            String response =  inputCommand.runCommand();
+            out.format(response + "\n");
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
