3085ac6e4b31bd063da54e04631ef4f819f9a680
Moein Arabi <moein.mo81@gmail.com>
2022-06-23 15:37:56 +0430

    Fixed bug

--- src/server/server.java
+++ src/server/server.java
@@ -9,15 +9,16 @@ import java.util.concurrent.Executors;
 public class server {
     public static void main(String[] args) {
         ExecutorService executorService = Executors.newFixedThreadPool(4);
-        
-        while (true) {
-            try {
-                ServerSocket serverSocket = new ServerSocket(5000);
+    
+        try {
+            ServerSocket serverSocket = new ServerSocket(5000);
+            
+            while (true) {
                 Socket socket = serverSocket.accept();
                 executorService.execute(new AcceptClient(socket));
-            } catch (IOException e) {
-                e.printStackTrace();
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
