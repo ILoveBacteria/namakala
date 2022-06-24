28e29e1d3d4f9893d38fb03b4b5c9400940f8a87
Moein Arabi <moein.mo81@gmail.com>
2022-06-24 11:11:30 +0430

    Changed sender person object to phone

--- lib/socket/socket.dart
+++ lib/socket/socket.dart
@@ -4,23 +4,21 @@ import 'dart:io';
 
 import 'package:namakala/socket/command.dart';
 
-import '../utilities/person.dart';
-
 class MySocket {
   final int port = 5000;
   final String host = "10.0.2.2";
-  final Person? sender;
+  final String? phoneSender;
   final Command command;
   final List<String> data;
 
-  MySocket(this.sender, this.command, this.data);
+  MySocket(this.phoneSender, this.command, this.data);
 
   Future<String> sendAndReceive() async {
     var socket = await Socket.connect(host, port);
     if (data.isNotEmpty) {
-      socket.writeln("${sender?.phone} ${command.name} ${data.join(";")}");
+      socket.writeln("$phoneSender ${command.name} ${data.join(";")}");
     } else {
-      socket.writeln("${sender?.phone} ${command.name}");
+      socket.writeln("$phoneSender ${command.name}");
     }
     await socket.flush();
     String response = await utf8.decoder.bind(socket).join();
