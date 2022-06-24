afa83b1d72b64ffd2f09f2717ae43fc32a73a495
Moein Arabi <moein.mo81@gmail.com>
2022-06-24 14:15:00 +0430

    Updated fromJson cunstroctor

--- lib/utilities/market.dart
+++ lib/utilities/market.dart
@@ -2,15 +2,19 @@ import 'package:namakala/utilities/person.dart';
 import 'package:namakala/utilities/product.dart';
 
 class Market {
-  Person owner;
-  String name;
+  late Person owner;
+  late String name;
   List<Product> products = [];
 
   Market(this.owner, this.name);
 
-  Market.fromJson(Map<String, dynamic> json)
-      : name = json['name'],
-        owner = Person.fromJson(json['owner']);
+  Market.fromJson(Map<String, dynamic> json) {
+    name = json['name'];
+    owner = Person.fromJson(json['owner']);
+    for (Map<String, dynamic> p in json['products']) {
+      products.add(Product.fromJson(p));
+    }
+  }
 
   Map<String, dynamic> toJson() => {
         'name': name,
