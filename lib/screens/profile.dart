770fae98dd87602371a94df5ebafd7130f7f5ae9
Moein Arabi <moein.mo81@gmail.com>
2022-06-24 15:28:28 +0430

    Added socket to Profile screen

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,149 +1,177 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
-import 'package:namakala/data/sample_data.dart';
+import 'package:namakala/data/user_data.dart';
 import 'package:namakala/screens/account_information.dart';
 import 'package:namakala/screens/my_market.dart';
 import 'package:namakala/screens/product_category.dart';
 import 'package:namakala/screens/purchase_history/cart_list.dart';
 import 'package:namakala/screens/sign_in.dart';
+import 'package:namakala/socket/command.dart';
+import 'package:namakala/socket/socket.dart';
 import 'package:namakala/utilities/font.dart';
+import 'package:namakala/utilities/person.dart';
 import 'package:namakala/widgets/button.dart';
 import 'package:namakala/widgets/screen_setting.dart';
 
-class Profile extends StatelessWidget {
+class Profile extends StatefulWidget {
   const Profile({Key? key}) : super(key: key);
 
   @override
+  State<Profile> createState() => _ProfileState();
+}
+
+class _ProfileState extends State<Profile> {
+  bool _dataReceived = false;
+  late Person person;
+
+  @override
   Widget build(BuildContext context) {
+    _getDataFromServer().then((value) {
+      person = value;
+      _dataReceived = true;
+      setState(() {});
+    });
+
     return ScreenSetting.initScreen(
       appBar: ScreenSetting.appBar(title: 'Profile'),
       context: context,
-      child: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      child: _dataReceived ? _buildMainScreen(context) : _buildWaitingScreen(),
+    );
+
+  }
+
+  Widget _buildMainScreen(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Row(
+            children: [
+              Container(
+                width: 100.0,
+                height: 100.0,
+                decoration: BoxDecoration(
+                  border: Border.all(),
+                  shape: BoxShape.circle,
+                ),
+                child: const Icon(
+                  Icons.person,
+                  size: 50.0,
+                ),
+              ),
+              const SizedBox(width: 20.0),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    '${person.firstname} ${person.lastname}',
+                    style: Font.styleSubtitle1(),
+                    overflow: TextOverflow.fade,
+                    maxLines: 1,
+                  ),
+                  const SizedBox(height: 5.0),
+                  Text(
+                    person.phone,
+                    style: Font.styleBody2(color: Colors.grey),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+        const SizedBox(height: 20.0),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30.0),
+          child: IntrinsicHeight(
             child: Row(
               children: [
-                Container(
-                  width: 100.0,
-                  height: 100.0,
-                  decoration: BoxDecoration(
-                    border: Border.all(),
-                    shape: BoxShape.circle,
-                  ),
-                  child: const Icon(
-                    Icons.person,
-                    size: 50.0,
-                  ),
+                _userActivity(title: 'Purchase', number: '${person.purchases.length}'),
+                const Spacer(),
+                const VerticalDivider(
+                  color: Colors.grey,
                 ),
-                const SizedBox(width: 20.0),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      '${SampleData.person.firstname} ${SampleData.person.lastname}',
-                      style: Font.styleSubtitle1(),
-                      overflow: TextOverflow.fade,
-                      maxLines: 1,
-                    ),
-                    const SizedBox(height: 5.0),
-                    Text(
-                      SampleData.person.phone,
-                      style: Font.styleBody2(color: Colors.grey),
-                    ),
-                  ],
-                )
+                const Spacer(),
+                _userActivity(title: 'Sold', number: '2'),
+                const Spacer(),
+                const VerticalDivider(
+                  color: Colors.grey,
+                ),
+                const Spacer(),
+                _userActivity(title: 'Products', number: '${person.market.products.length}'),
               ],
             ),
           ),
-          const SizedBox(height: 20.0),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 30.0),
-            child: IntrinsicHeight(
-              child: Row(
-                children: [
-                  _userActivity(title: 'Purchase', number: '${SampleData.person.purchases.length}'),
-                  const Spacer(),
-                  const VerticalDivider(
-                    color: Colors.grey,
-                  ),
-                  const Spacer(),
-                  _userActivity(title: 'Sold', number: '2'),
-                  const Spacer(),
-                  const VerticalDivider(
-                    color: Colors.grey,
-                  ),
-                  const Spacer(),
-                  _userActivity(title: 'Products', number: '${SampleData.person.market?.products.length ?? 0}'),
-                ],
-              ),
-            ),
-          ),
-          const SizedBox(height: 15.0),
-          Container(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              children: [
-                _button(
-                  text: 'Account Information',
-                  icon: Icons.account_circle_outlined,
-                  onPressed: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) => const Account(),
-                      ),
-                    );
-                  },
-                ),
-                const SizedBox(height: 20.0),
-                _button(
-                  text: 'Purchase History',
-                  icon: Icons.history,
-                  onPressed: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) => PurchasedCartList(),
-                      ),
-                    );
-                  },
-                ),
-                const SizedBox(height: 20.0),
-                _button(
-                  text: 'My Market',
-                  icon: Icons.store_outlined,
-                  onPressed: () => Navigator.of(context).push(
+        ),
+        const SizedBox(height: 15.0),
+        Container(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              _button(
+                text: 'Account Information',
+                icon: Icons.account_circle_outlined,
+                onPressed: () {
+                  Navigator.of(context).push(
                     MaterialPageRoute(
-                      builder: (context) => const MyMarket(),
+                      builder: (context) => const Account(),
                     ),
+                  );
+                },
+              ),
+              const SizedBox(height: 20.0),
+              _button(
+                text: 'Purchase History',
+                icon: Icons.history,
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => PurchasedCartList(),
+                    ),
+                  );
+                },
+              ),
+              const SizedBox(height: 20.0),
+              _button(
+                text: 'My Market',
+                icon: Icons.store_outlined,
+                onPressed: () => Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => const MyMarket(),
                   ),
                 ),
-                const SizedBox(height: 20.0),
-                _button(
-                  text: 'Favorites',
-                  icon: Icons.favorite_outline,
-                  onPressed: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) => const ProductCategory(),
-                        settings: const RouteSettings(arguments: 'Favorites'),
-                      ),
-                    );
-                  },
-                ),
-                Button.separate(),
-                Button.signOut(
-                  onPressed: () {
-                    Navigator.of(context).pushAndRemoveUntil(
-                      MaterialPageRoute(builder: (context) => const SignIn()),
-                      (Route<dynamic> route) => false,
-                    );
-                  },
-                ),
-              ],
-            ),
+              ),
+              const SizedBox(height: 20.0),
+              _button(
+                text: 'Favorites',
+                icon: Icons.favorite_outline,
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => const ProductCategory(),
+                      settings: const RouteSettings(arguments: 'Favorites'),
+                    ),
+                  );
+                },
+              ),
+              Button.separate(),
+              Button.signOut(
+                onPressed: () {
+                  Navigator.of(context).pushAndRemoveUntil(
+                    MaterialPageRoute(builder: (context) => const SignIn()),
+                        (Route<dynamic> route) => false,
+                  );
+                },
+              ),
+            ],
           ),
-        ],
-      ),
+        ),
+      ],
     );
+  }
+
+  Widget _buildWaitingScreen() {
+    return Container();
   }
 
   Widget _button({
@@ -201,5 +229,11 @@ class Profile extends StatelessWidget {
         ),
       ],
     );
+  }
+
+  Future<Person> _getDataFromServer() async {
+    MySocket socket = MySocket(UserData.phone, Command.profile, []);
+    String response = await socket.sendAndReceive();
+    return Person.fromJson(jsonDecode(response));
   }
 }
