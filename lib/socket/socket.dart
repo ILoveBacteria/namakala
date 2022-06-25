7edd2fc2bd42269a25c7058c8a165b225d22de4e
Moein Arabi <moein.mo81@gmail.com>
2022-06-25 17:02:01 +0430

    Added raw byte response for getting images

--- lib/socket/socket.dart
+++ lib/socket/socket.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'dart:core';
 import 'dart:io';
+import 'dart:typed_data';
 
 import 'package:namakala/socket/command.dart';
 
@@ -14,15 +15,30 @@ class MySocket {
   MySocket(this.phoneSender, this.command, this.data);
 
   Future<String> sendAndReceive() async {
+    Socket socket = await _send();
+    String response = await utf8.decoder.bind(socket).join();
+    await socket.close();
+    return response;
+  }
+
+  Future<Uint8List?> sendAndReceiveRaw() async {
+    Uint8List? byte;
+    Socket socket = await _send();
+    socket.listen((event) async {
+      byte = event;
+      await socket.close();
+    });
+
+    return byte;
+  }
+
+  Future<Socket> _send() async {
     var socket = await Socket.connect(host, port);
     if (data.isNotEmpty) {
       socket.writeln("$phoneSender ${command.name} ${data.join(";")}");
     } else {
       socket.writeln("$phoneSender ${command.name}");
     }
-    await socket.flush();
-    String response = await utf8.decoder.bind(socket).join();
-    await socket.close();
-    return response;
+    return socket;
   }
 }
