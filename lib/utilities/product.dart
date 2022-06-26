09a5710b2b762c1e66e75d95c2166775142f9f4a
Moein Arabi <moein.mo81@gmail.com>
2022-06-26 13:01:42 +0430

    Changed in fromJson cunstroctor

--- lib/utilities/product.dart
+++ lib/utilities/product.dart
@@ -1,19 +1,21 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 import 'package:namakala/utilities/market.dart';
 
 class Product {
   late int id;
   late String name;
-  late String image;
+  late Uint8List image;
   late String detail;
   late int price;
   late String category;
   late Market market;
-  int? count;
+  late int count;
   double score = 0;
   int _countVoters = 0;
-  late List<Color> color;
-  late List<String> size;
+  late List<Color> color = [];
+  late List<String> size = [];
 
   Product(this.name, this.image, this.price, this.category, this.detail,
       this.color, this.size, this.market);
@@ -21,12 +23,17 @@ class Product {
   Product.fromJson(Map<String, dynamic> json) {
     id = json['id'];
     name = json['name'];
-    image = json['image'];
     detail = json['detail'];
     price = json['price'];
     category = json['category'];
     market = Market.fromJson(json['market']);
-    size = json['size'];
+
+    List<dynamic> dynamicSizes = json['size'];
+    for (var i in dynamicSizes) {
+      size.add(i);
+    }
+
+    count = json['count'];
 
     for (int value in json['color']) {
       color.add(Color(value));
@@ -35,6 +42,7 @@ class Product {
 
   Map<String, dynamic> toJson() => {
         'id': id,
+        'category':category,
       };
 
   @override
