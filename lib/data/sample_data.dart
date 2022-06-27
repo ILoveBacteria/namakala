3a99cb41539e6ceccfcf7b28262d1cf788e07a04
Moein Arabi <moein.mo81@gmail.com>
2022-06-27 12:48:24 +0430

    Removed product sample data

--- lib/data/sample_data.dart
+++ lib/data/sample_data.dart
@@ -1,74 +1,7 @@
-import 'package:flutter/material.dart';
-
-import '../utilities/market.dart';
 import '../utilities/person.dart';
-import '../utilities/product.dart';
 
 class SampleData {
   static Person person = Person('Moein', 'Arabi', '09234567891', 'abcd1234');
 
   static List<Person> persons = [Person('Moein', 'Arabi', '09234567891', 'abcd1234'), Person('Mr', 'Arabi', '09234567892', 'abcde1234')];
-
-  static final Map<Product, int> products = <Product, int>{
-    Product(
-      'iPhone 13 pro max',
-      'assets/images/iphone.png',
-      999,
-      'Mobiles',
-      'This product is in Test!',
-      [Colors.blue, Colors.yellow],
-      ['128GB', '256GB', '512GB'],
-      Market('Digikala'),
-    ): 3,
-    Product(
-      'iPhone 13',
-      'assets/images/iphone13.png',
-      899,
-      'Mobiles',
-      'This product is in Test!',
-      [Colors.blue, Colors.black],
-      ['128GB', '256GB'],
-      Market('Digikala'),
-    ): 2,
-    Product(
-      'iPhone 13 pro',
-      'assets/images/iphone.png',
-      999,
-      'Mobiles',
-      'This product is in Test!',
-      [Colors.pink, Colors.green],
-      ['128GB', '256GB'],
-      Market('Digikala'),
-    ): 1,
-    Product(
-      'iPhone 13 mini',
-      'assets/images/iphone13.png',
-      899,
-      'Mobiles',
-      'This product is in Test!',
-      [Colors.blue],
-      ['128GB', '256GB'],
-      Market('Digikala'),
-    ): 0,
-    Product(
-      'Macbook Air',
-      'assets/images/macbook.png',
-      999,
-      'Laptops',
-      'This product is in Test!',
-      [Colors.grey],
-      ['default'],
-      Market('Digikala'),
-    ): 3,
-    Product(
-      'MacBook Pro M1',
-      'assets/images/macbook.png',
-      1199,
-      'Laptops',
-      'This product is in Test!',
-      [Colors.grey],
-      ['default'],
-      Market('Digikala'),
-    ): 2,
-  };
 }
