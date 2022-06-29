6ed2202fb65608e5d8b6dc1aa2295a19f4f35902
Moein Arabi <moein.mo81@gmail.com>
2022-06-29 11:54:21 +0430

    Added a new argument

--- lib/utilities/arguments.dart
+++ lib/utilities/arguments.dart
@@ -2,8 +2,13 @@ import 'person.dart';
 import 'product.dart';
 
 class Arguments {
-  Person person;
-  Product product;
+  Person? person;
+  Product? product;
+  String? category;
 
   Arguments(this.person, this.product);
+
+  Arguments.favoriteCategory(this.person, this.category);
+
+  Arguments.category(this.category);
 }
