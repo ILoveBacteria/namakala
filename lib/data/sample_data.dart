63f02e1409ca981dcabce2bd1a15f58da621b36a
Moein Arabi <moein.mo81@gmail.com>
2022-07-05 23:52:57 +0430

    Deleted sample_data.dart

--- lib/data/sample_data.dart
+++
@@ -1,7 +0,0 @@
-import '../utilities/person.dart';
-
-class SampleData {
-  static Person person = Person('Moein', 'Arabi', '09234567891', 'abcd1234');
-
-  static List<Person> persons = [Person('Moein', 'Arabi', '09234567891', 'abcd1234'), Person('Mr', 'Arabi', '09234567892', 'abcde1234')];
-}
